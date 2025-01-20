f8ef5536aa621103caabdb0bbc61f89a203cd341
Heinz Beck <heinz.beck@hella-gutmann.com>
2025-01-20 11:01:59 +0100

    - add createStar function - add createStars function - add camera animation into to starfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+const handleResize = (
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.WebGLRenderer,
+) => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const aspect = width / height;
+
+  camera.aspect = aspect;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(width, height);
+};
+
+const createStar = (color: number = 0xffffff, size: number = 0.1) => {
+  // create geometry
+  const geometry = new THREE.SphereGeometry(size, 8, 8);
+
+  // create material
+  const material = new THREE.MeshBasicMaterial({ color });
+
+  // create mesh
+  return new THREE.Mesh(geometry, material);
+};
+
+const createStars = (count: number = 100, distance: number = 200) => {
+  const group = new THREE.Group();
+
+  for (let i = 0; i < count; i += 1) {
+    const star = createStar();
+
+    const [x, y, z] = Array.from({ length: 3 }, () => {
+      const randomNumber = Math.random() - 0.5; // random number between -0.5 and 0.5
+      return randomNumber * distance;
+    });
+
+    star.position.set(x, y, z);
+    group.add(star);
+  }
+  return group;
+};
 
 function App() {
   useEffect(() => {
@@ -11,19 +54,12 @@ function App() {
       75,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000,
+      100,
     );
     camera.position.z = 5; // Make sure the camera is in a position to view the box
 
-    // create geometry
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-    // create material
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-
-    // create mesh
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    const stars = createStars(10000, 200);
+    scene.add(stars);
 
     // create renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -38,12 +74,29 @@ function App() {
 
     const animate = () => {
       requestAnimationFrame(animate);
-      mesh.rotation.x += 0.01; // Rotate the mesh for some animation
-      mesh.rotation.y += 0.01;
+      // stars.rotation.x += 0.01; // Rotate the star for some animation
+      // stars.rotation.y += 0.01;
+
+      // animate camera
+      camera.position.z -= 0.1;
+
+      if (camera.position.z < -50) {
+        camera.position.z = 100;
+      }
+
       renderer.render(scene, camera);
     };
 
     animate();
+
+    window.addEventListener('resize', () => {
+      handleResize(camera, renderer);
+    });
+
+    // create controls
+    const controlls = new OrbitControls(camera, renderer.domElement);
+    controlls.enableDamping = true;
+    controlls.dampingFactor = 0.25;
 
     // Clean up on unmount
     return () => {
